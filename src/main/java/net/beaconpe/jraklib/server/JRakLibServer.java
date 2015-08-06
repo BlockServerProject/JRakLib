d8bae60b069111a0ab693a8e155dbc6a2ace379f
jython234 <jython234@hotmail.com>
2015-08-06 16:07:25 -0500

    Add license header maven plugin.

--- src/main/java/net/beaconpe/jraklib/server/JRakLibServer.java
+++ src/main/java/net/beaconpe/jraklib/server/JRakLibServer.java
@@ -1,23 +1,21 @@
-/*
-   JRakLib networking library.
-   This software is not affiliated with RakNet or Jenkins Software LLC.
-   This software is a port of PocketMine/RakLib <https://github.com/PocketMine/RakLib>.
-   All credit goes to the PocketMine Project (http://pocketmine.net)
- 
-   Copyright (C) 2015 BlockServerProject & PocketMine team
+/**
+ * JRakLib is not affiliated with Jenkins Software LLC or RakNet.
+ * This software is a port of RakLib https://github.com/PocketMine/RakLib.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of JRakLib.
+ *
+ * JRakLib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JRakLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JRakLib.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.beaconpe.jraklib.server;
 
