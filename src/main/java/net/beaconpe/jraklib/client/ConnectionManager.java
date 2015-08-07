e01c2d83df74410e8a9c19012efee322c79ad744
jython234 <jython234@hotmail.com>
2015-08-07 18:11:59 -0500

    Add remaining license headers.

--- src/main/java/net/beaconpe/jraklib/client/ConnectionManager.java
+++ src/main/java/net/beaconpe/jraklib/client/ConnectionManager.java
@@ -1,3 +1,22 @@
+/**
+ * JRakLib is not affiliated with Jenkins Software LLC or RakNet.
+ * This software is a port of RakLib https://github.com/PocketMine/RakLib.
+
+ * This file is part of JRakLib.
+ *
+ * JRakLib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JRakLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JRakLib.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.beaconpe.jraklib.client;
 
 import static net.beaconpe.jraklib.JRakLib.getAddressFromString;
